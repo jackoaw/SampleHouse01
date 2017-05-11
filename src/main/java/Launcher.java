be554ee17d1c1d706ad2947baea5bb94ec360add
jackoaw <jackoaw@vt.edu>
2017-05-11 11:58:00 -0400

    made certificate available

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -7,7 +7,7 @@
 
     or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
  */
-import sample.SampleHaus;
+
 
 import org.apache.log4j.BasicConfigurator;
 import org.eclipse.jetty.server.Connector;
@@ -26,6 +26,8 @@ import session.SessionSpeechlet;
 import com.amazon.speech.Sdk;
 import com.amazon.speech.speechlet.Speechlet;
 import com.amazon.speech.speechlet.servlet.SpeechletServlet;
+
+import sample.SampleHaus;
 
 /**
  * Shared launcher for executing all sample skills within a single servlet container.
